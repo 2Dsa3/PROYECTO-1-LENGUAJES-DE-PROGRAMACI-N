45f7125d091380a8995cd4f1cf0ad60d587460f1
juarseve <juarseve@espol.edu.ec>
2024-11-11 20:53:22 -0500

    basic lex-analizer impl

---
+++ PROYECTO I/Analizador_Lexico.py
@@ -0,0 +1,81 @@
+import ply.lex as lex
+
+
+reserved = {'if': 'IF', 'else': 'ELSE', 'while': 'WHILE', 'for': 'FOR', 'input': 'INPUT', 'print':'PRINT'}
+
+# List of token names.   This is always required
+tokens = (
+             'NUMBER',
+             'PLUS',
+             'MINUS',
+             'TIMES',
+             'DIVIDE',
+             'LPAREN',
+             'RPAREN',
+             'VARIABLE',
+             'HASH',
+             'FLOAT'
+         ) + tuple(reserved.values())
+
+# Regular expression rules for simple tokens
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_HASH = r'\#'
+
+
+
+
+def t_FLOAT(t):
+    r'[+-]?([0-9]*[.])?[0-9]+'
+    t.type = reserved.get(t.value, 'FLOAT')
+    return t
+
+
+def t_VARIABLE(t):
+    r'[a-zA-Z_]\w*'
+    t.type = reserved.get(t.value, 'VARIABLE')
+    return t
+
+# A regular expression rule with some action code
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore = ' \t'
+
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+#
+# Build the lexer
+lexer = lex.lex()
+
+data = '''
+3 + 4 * 10
+  + -20 *2 If print(4) 4.3 # input(t)
+'''
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok:
+        break  # No more input
+    print(tok)
